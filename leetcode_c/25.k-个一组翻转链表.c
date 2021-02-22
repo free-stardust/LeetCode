ed25d9bc17635f33f9a0cb710cec7ee0db3ced60
free-stardust <gblvso@163.com>
2021-02-22 13:56:45 +0800

    add leetcode25

---
+++ leetcode_c/25.k-个一组翻转链表.c
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=25 lang=c
+ *
+ * [25] K 个一组翻转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* reverseKGroup(struct ListNode* head, int k){
+
+}
+// @lc code=end
+
