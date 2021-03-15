6337019a2e2d8fe680221d8f2af338481bf86ec4
free-stardust <gblvso@163.com>
2021-03-15 11:52:28 +0800

    2021.03.15 add answer35

---
+++ LeetCode-C/35.搜索插入位置.c
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=c
+ *
+ * [35] 搜索插入位置
+ *
+ * https://leetcode-cn.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (47.04%)
+ * Likes:    847
+ * Dislikes: 0
+ * Total Accepted:    335.9K
+ * Total Submissions: 714.3K
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 
+ * 你可以假设数组中无重复元素。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ * 
+ * 
+ */
+
+// @lc code=start
+
+
+int searchInsert(int* nums, int numsSize, int target){
+    if (nums == NULL || numsSize <= 0)
+        return 0;
+    else
+    {
+        int low = 0, high = numsSize - 1, mid = 0;
+        while (low <= high)
+        {
+            mid = (low + high) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else 
+                high = mid - 1;
+        }   
+        return low;     
+    }
+}
+// @lc code=end
+
