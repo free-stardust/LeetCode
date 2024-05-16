3a5898fae2d0f39309969ba1f4c6d21870e17392
Stardust <gblvso@163.com>
2024-05-17 01:51:24 +0800

    update by master 202405170151

--- cpp/1.two-sum.cpp
+++ cpp/1.two-sum.cpp
@@ -92,14 +92,14 @@ public:
     }
 };
 
-int main() {
-    vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
-    Solution solu;
-    vector<int> result = solu.twoSum(nums, target);
-    cout << "[" << result[0] << "," << result[1] << "]" << endl;
-    return 0;
-}
+// int main() {
+//     vector<int> nums = {2, 7, 11, 15};
+//     int target = 9;
+//     Solution solu;
+//     vector<int> result = solu.twoSum(nums, target);
+//     cout << "[" << result[0] << "," << result[1] << "]" << endl;
+//     return 0;
+// }
 // @lc code=end
 
 
