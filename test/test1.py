810ae178f4fb7ddc4cfe15a6b41ef50e5d2d678c
Stardust <gblvso@163.com>
2025-04-28 15:19:28 +0800

    update by master 202504281519

---
+++ test/test1.py
@@ -0,0 +1,23 @@
+# Q: 给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串 的长度
+# example: abcabcbb
+
+
+def func(s):
+    res = 0
+    sub_s = ""
+
+    for c in s:
+        if c not in sub_s:
+            sub_s += c
+        else:
+            sub_s = sub_s[sub_s.index(c) + 1:] + c
+
+        res = max(len(sub_s), res)
+
+    return res
+
+
+ss = ["abcabcbb", "abcabcd", "abcde"]
+for s in ss:
+    res = func(s)
+    print("Result: " + str(res))
